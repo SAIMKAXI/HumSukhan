d835788f3da026ca33f411d73a326888cfb5a8c4
SAIMKAXI <saim2kazi006@gmail.com>
2026-09-01 21:17:23 +0500

    refactor(environmental): route feature through module API

--- humsukhan/lib/modules/environmental_alerts/environmental_alerts.dart
+++ humsukhan/lib/modules/environmental_alerts/environmental_alerts.dart
@@ -1,6 +1,9 @@
-/// Public entry point for Environmental Alerts.
-export '../../screens/environmental_screen.dart';
-export '../../providers/environmental_provider.dart';
-export '../../services/sound_detection_service.dart';
-export '../../services/environmental_monitoring_bridge.dart';
-export '../../services/alert_service.dart';
+/// Public API for Environmental Alerts.
+export 'screens/environmental_screen.dart';
+export 'providers/environmental_provider.dart';
+export 'services/sound_detection_service.dart';
+export 'services/environmental_monitoring_bridge.dart';
+export 'services/alert_service.dart';
+export 'services/audio_model_manager.dart';
+export 'services/sherpa_audio_tagger.dart';
+export 'services/notification_service.dart';
