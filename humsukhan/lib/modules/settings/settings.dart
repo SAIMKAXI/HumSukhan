4701f093a9229a89e72be170355596cbdb911c91
SAIMKAXI <saim2kazi006@gmail.com>
2026-09-01 21:17:30 +0500

    refactor(settings): route feature through module API

--- humsukhan/lib/modules/settings/settings.dart
+++ humsukhan/lib/modules/settings/settings.dart
@@ -1,4 +1,4 @@
-/// Public entry point for Settings.
-export '../../screens/settings_screen.dart';
-export '../../providers/settings_provider.dart';
+/// Public API for Settings.
+export 'screens/settings_screen.dart';
+export 'providers/settings_provider.dart';
 export '../../theme/app_theme.dart';
