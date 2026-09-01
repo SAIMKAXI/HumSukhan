e8d1225ffe4868a4a5cf5fd986742ddbe15edf44
SAIMKAXI <saim2kazi006@gmail.com>
2026-09-01 20:41:05 +0500

    refactor: add environmental alerts module boundary

---
+++ humsukhan/lib/modules/environmental_alerts/environmental_alerts.dart
@@ -0,0 +1,6 @@
+/// Public entry point for Environmental Alerts.
+export '../../screens/environmental_screen.dart';
+export '../../providers/environmental_provider.dart';
+export '../../services/sound_detection_service.dart';
+export '../../services/environmental_monitoring_bridge.dart';
+export '../../services/alert_service.dart';
