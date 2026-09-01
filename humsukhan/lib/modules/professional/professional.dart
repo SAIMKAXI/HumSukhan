19b9c97e186045ff32fad41de204145fe0d3b249
SAIMKAXI <saim2kazi006@gmail.com>
2026-09-01 21:17:16 +0500

    refactor(professional): route feature through module API

--- humsukhan/lib/modules/professional/professional.dart
+++ humsukhan/lib/modules/professional/professional.dart
@@ -1,6 +1,6 @@
-/// Public entry point for Professional Mode.
-export '../../screens/professional_screen.dart';
-export '../../screens/session_live_screen.dart';
-export '../../screens/session_detail_screen.dart';
-export '../../providers/professional_provider.dart';
+/// Public API for Professional Mode.
+export 'screens/professional_screen.dart';
+export 'screens/session_live.dart';
+export 'screens/session_detail.dart';
+export 'providers/professional_provider.dart';
 export '../../models/models.dart' show ProfessionalSession, Transcript, Caption;
