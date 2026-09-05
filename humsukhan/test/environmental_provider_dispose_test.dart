a4515cff7368814daacbbd44201307a029aff16f
SAIMKAXI <saim2kazi006@gmail.com>
2026-09-06 03:22:48 +0500

    test(environmental): initialize Flutter binding for dispose regression

--- humsukhan/test/environmental_provider_dispose_test.dart
+++ humsukhan/test/environmental_provider_dispose_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:humsukhan/providers/environmental_provider.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   test('environmental provider can be disposed without notification failures', () {
     final provider = EnvironmentalProvider();
     provider.dispose();
