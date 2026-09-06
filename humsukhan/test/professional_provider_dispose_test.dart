3bfb4904c6b1d8a80d7adb23d4666c7cec90ee05
SAIMKAXI <saim2kazi006@gmail.com>
2026-09-06 08:04:41 +0500

    test(professional): initialize Flutter binding before provider lifecycle test

--- humsukhan/test/professional_provider_dispose_test.dart
+++ humsukhan/test/professional_provider_dispose_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:humsukhan/providers/professional_provider.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   test('notifyListeners is safe after provider disposal', () {
     final provider = ProfessionalProvider();
     provider.dispose();
